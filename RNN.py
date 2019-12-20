a8a42b04fa4190999448a8c55d3a202cb3f13124
minsu3007 <55453151+minsu3007@users.noreply.github.com>
2019-12-20 19:00:53 +0900

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -5,43 +5,46 @@ from tensorflow.keras.models import Sequential
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
-def create_window(data,window_size):
-    window_data=[]
-    for i in range(data.shape[0]-window_size+1):
-        window_data.append(data[i:i+window_size,:])
-    return np.array(window_data)
-window_size=15
+def create_window(data,window_size,features):
+    window_data=np.array([])
+    for i in range(data.shape[0]-window_size):
+        window_data=np.append(window_data,data[i:i+window_size,:])
+    window_data=window_data.reshape(-1,window_size,features)
+    return window_data
+
+window_size=7
 train=pd.read_csv('data/HYMTF.csv')
 
-data=train['Close'].values.reshape(-1,1)
+x_data=train['Close'].values.reshape(-1,1)
+x_data=np.append(x_data,train['Volume'].values.reshape(-1,1),axis=1)
+x_data=np.append(x_data,train['Adj Close'].values.reshape(-1,1),axis=1)
 
 scaler=MinMaxScaler()
-scaled=scaler.fit(data)
-scaled_data=scaled.fit_transform(data)
+x_scaled=scaler.fit(x_data)
+x_data=x_scaled.fit_transform(x_data)
 
-data=create_window(scaled_data,window_size)
-x_data=data[:,:window_size-1,:]
-y_data=data[:,window_size-1:,:]
-y_data=y_data.reshape(-1,1)
+x_data=create_window(x_data,window_size,features=3)
+
+train_target=train.shift(-window_size)
+y_data=train_target['Close'][:-window_size].values.reshape(-1,1)
+y_scaled=scaler.fit(y_data)
+y_data=y_scaled.fit_transform(y_data)
 '''
 return_sequences=True : ,x.shape==y.shape
 return_sequences=False : x.shape=3,y.shape=2
 '''
 model=Sequential()
-model.add(LSTM(64,batch_input_shape=(1,x_data.shape[1],x_data.shape[2]),return_sequences=True,stateful=True))
-model.add(LSTM(128))
-model.add(Dense(64,activation='relu'))
+model.add(LSTM(6,batch_input_shape=(1,x_data.shape[1],x_data.shape[2]),return_sequences=True,stateful=True))
+model.add(LSTM(12,stateful=True))
 model.add(Dense(1,activation='sigmoid'))
 
-model.compile(loss='mse',optimizer='adam',metrics=['acc'])
+model.compile(loss='mse',optimizer='rmsprop',metrics=['acc'])
 model.fit(x_data,y_data,epochs=5)
 
-y_data=scaled.inverse_transform(y_data)
-predict=model.predict(x_data)
-predict=scaled.inverse_transform(predict)
+y_data=y_scaled.inverse_transform(y_data)
+predict=model.predict(x_data,verbose=1)
+predict=y_scaled.inverse_transform(predict)
 
 plt.plot(y_data[:500,:],color='B')
 plt.plot(predict[:500,:],color='R')
-
-
 
